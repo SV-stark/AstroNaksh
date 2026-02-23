d5a6c4f54c66d1ace889677510e296f48d5e06fe
SV-stark <svstark@proton.me>
2026-02-24 00:23:19 +0530

    build(android): fix kotlin DSL for AGP 9.0 compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,10 +14,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.astronaksh.stark"
@@ -47,4 +43,10 @@ android {
 
 flutter {
     source = "../.."
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }

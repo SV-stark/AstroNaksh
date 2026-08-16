666952a075eaa1aa89fb49e6055592422bedfdef
SV-Stark <Suyashsverma335@gmail.com>
2026-08-16 17:52:07 +0530

    fix(build): safely configure subprojects compileSdkVersion before app evaluation

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,13 +14,8 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
 
-subprojects {
-    afterEvaluate {
+    val configureAndroid = {
         if (project.hasProperty("android")) {
             val android = project.extensions.findByName("android")
             if (android is com.android.build.gradle.BaseExtension) {
@@ -28,6 +23,16 @@ subprojects {
             }
         }
     }
+
+    if (state.executed) {
+        configureAndroid()
+    } else {
+        afterEvaluate { configureAndroid() }
+    }
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
